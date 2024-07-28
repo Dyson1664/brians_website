e0122d41d59944ec492261f2d2dd357d1844cc2f
david reilly <davidreilly02@gmail.com>
2024-07-28 10:32:16 +0100

    changed landing page

--- b_app/urls.py
+++ b_app/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('contact/', views.contact, name='views'),
-    path('index/', views.index, name='index')
 ]
